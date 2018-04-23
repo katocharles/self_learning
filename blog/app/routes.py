c3abe81279c272bca3a24c13bd3c6aee2cf4426a
Kato Charles <ckcloudia@gmail.com>
2018-04-23 13:06:51 +0300

    record time of last visit

--- blog/app/routes.py
+++ blog/app/routes.py
@@ -1,10 +1,18 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app.models import User
-from app.forms import RegistrationForm
+from datetime import datetime
+
+#record time of last visit
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
 
 @app.route('/')
 @app.route('/index')
